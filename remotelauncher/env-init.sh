1eb030757661f95e2b5d979145cee27cb27ffc96
epotters <epotters@rti.com>
2021-10-19 12:20:12 -0600

    Scenario remote laucher

---
+++ remotelauncher/env-init.sh
@@ -0,0 +1,9 @@
+################################################################################################################################
+# https://github.com/katacoda-scenarios/kubernetes-bootcamp-scenarios/blob/5efe0d9078a0e9cd10b3a11bca124ab76e794d8f/9/env-init.sh
+#########################################################################################################################################
+
+
+ssh root@host01 "git clone https://github.com/Slyracoon23/remotelauncher-demo.git"
+# ssh root@host01 "docker pull open-liberty:latest"
+
+# ssh root@host01 "while [ \`minikube status &>/dev/null; echo \$?\` -ne 0 ]; do sleep 1; done && kubectl create deployment kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1 --port=8080 && echo done >> /opt/katacoda-completed"
